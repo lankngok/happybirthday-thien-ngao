90687dcfbd97b78d1df21c4abec886711f032733
Be Lank Cute <105548698+lankngok@users.noreply.github.com>
2022-10-31 21:27:10 +0700

    Add files via upload

---
+++ php01/index.php
@@ -0,0 +1,141 @@
+<?php
+$name = 'Lank ngao'; // string
+$age = 19; //int
+$sex = true; // bool
+$price = 34.5;//float
+$mang = [1,2,'a'];
+echo'</pre>';
+echo $name;
+echo $age;
+var_dump($name);
+var_dump($age);
+var_dump($price);
+print_r($sex);
+print_r($mang);
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="text-danger text-center">Happy birthday</h1>
+    <main class="container">
+        <div class="d-flex align-items-center p-3 my-3 text-dark-50 bg-purple rounded shadow-sm">
+            <img class="mr-3" src="https://hinhnen123.com/wp-content/uploads/2021/06/hinh-anh-avatar-dep-nhat-18.jpg" alt="" width="60" height="60">
+            <div class="lh-1">
+                <h5 class="mb-1 text-dark lh-2">Lank Tran</h5>
+                <small class="text-sm-left lh-base font-normal text-lowercase text-decoration-none text-reset">
+                    Happy birthday thien ngao's ngo's chuc bae co ngay sinh nhat that vui ve va hanh phuc , mong rat nhiu niem vui va su thieu nang se den ben be :3
+                </small>
+            </div>
+        </div>
+
+        <div class="d-flex align-items-center p-3 my-3 text-dark-50 bg-purple rounded shadow-sm">
+            <img class="mr-3" src="https://avatars.githubusercontent.com/u/30625599?v=4" alt="" width="60" height="60">
+            <div class="lh-1">
+                <h5 class="mb-1 text-dark lh-2">Lank Tran</h5>
+                <small class="text-sm-left lh-base font-normal text-lowercase text-decoration-none text-reset">
+                    Hãy luôn giữ nét baby và giọng cười trời cho của anh nha,một anh chàng baby đáng…đánh đòn.
+                </small>
+            </div>
+        </div>
+
+        <div class="d-flex align-items-center p-3 my-3 text-dark-50 bg-purple rounded shadow-sm">
+            <img class="mr-3" src="https://pbs.twimg.com/profile_images/1337014224/75840_466991658253_80329313253_5550448_4696409_n_400x400.jpg" alt="" width="60" height="60">
+            <div class="lh-1">
+                <h5 class="mb-1 text-dark lh-2">Lank Tran</h5>
+                <small class="text-sm-left lh-base font-normal text-lowercase text-decoration-none text-reset">
+                    Hôm nay không như ngày hôm qua, hôm nay là một ngày đặc biệt, là ngày mà một thiên thần đáng yêu đã có mặt trên thế giới cách đây 19 năm. Luôn mỉm cười và may mắn nhé.
+                </small>
+            </div>
+        </div>
+
+    </main>
+
+
+
+    <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="mask flex-center">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-md-7 col-12 order-md-1 order-2">
+                                <h4>Thien NV</h4>
+                                <p>He is Thien (Lee Khac Thien hay con goi la Thien NV ) mac du nhin mat ko may uy tin nhung thanh nien nay la mot pro trong lang code . anh ay da tro thanh nhan vien nha truong nam 18 tuoi va dang la team leader tren trung tam phan mem . Thanh nien tre den tu Chuong Mi - Ha Tay . And now 31-10-2022 sinh nhat cua a ay , chuc a ay co nhiu niem vui va thanh cong trong tuong lai , chuc chang trai 19 tuoi luon thieu nang hoa :3 </p> <br> <a href="#">Read more</a>
+                            </div>
+                            <div class="col-md-5 col-12 order-md-2 order-1"><img src="113.jpg" class="mx-auto" alt="slide"></div>
+                        </div>
+                    </div>
+                    <div class=" mt-5 ">
+                        <div class=" col-md-6 ml-12">
+                            <video src="117.mp4" width="700" height="400" controls>
+                                Your browser does not support the <video> element.
+                                </video>
+                        </div>
+                    </div>
+
+                    <div class="container mt-5">
+                        <div class="row align-items-center">
+                            <div class="col-md-5 col-12 order-md-2 order-1">
+                                <h4>Lank Tran</h4>
+                                <p>Hi ! I'm lank ( lank ngao's ngo , thieu nang but tu tin and vui ve , giau nang luong) I'm from Hai Duong and i'm a student in C2110I1 Back Khoa Aptech . Mac du hoi thieu nang va bo lao nhung nang dong va vui tinh . Chao mung ae den voi ng con Hai Duong . Voi nhung dong code dep tu trong xuong , Lank luon code trong tam trang vui la chinh , dame la phu . Chuc ae luon tim dc dame trong bo mon code hack nao nhung vo cung giai tri :V</p> <br> <a href="#">Read more</a>
+                            </div>
+                            <div class="col-md-7 col-12 order-md-1 order-2"><img src="114.jpg" class="mx-auto" alt="slide"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Lank iu Thien</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Lank ngao</a>
+                            </li>
+                            <!-- <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li> -->
+                            <li class="nav-item dropup">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-expanded="false">Thien</a>
+                                <ul class="dropdown-menu" aria-labelledby="dropdown10">
+                                    <li><a class="dropdown-item" href="#">Thien ngao</a></li>
+                                    <li><a class="dropdown-item" href="#">Thien ngok</a></li>
+                                    <li><a class="dropdown-item" href="#">Thien thieu nang</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
